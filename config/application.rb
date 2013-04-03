7c12d788ba8baa3d4a17c58796b343137e34ba02
jirilanger <jiri.langer@spotfizz.com>
2013-04-03 23:01:28 +0200

    Fonts, images added

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,6 @@ module SpotfizzCom
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.paths << "#{Rails.root}/app/assets/fonts
+      
   end
 end
